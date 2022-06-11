392f38e2c3e866f1dad7e7e2749c9e860428d602
michellebritoo <michele.brito17@gmail.com>
2022-06-11 00:14:37 -0300

    💄Update MainActivity base layout

--- app/src/main/java/br/com/michellebrito/devhub/MainActivity.kt
+++ app/src/main/java/br/com/michellebrito/devhub/MainActivity.kt
@@ -3,26 +3,38 @@ package br.com.michellebrito.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.michellebrito.devhub.ui.theme.DevhubTheme
+import androidx.compose.ui.unit.dp
+import br.com.michellebrito.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DevhubTheme {
-                // A surface container using the 'background' color from the theme
+            DevHubTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ShowDetailsAccount(
+                        name = "Michelle Brito",
+                        userName = "michellebritoo",
+                        description = "Android dev"
+                    )
                 }
             }
         }
@@ -30,14 +42,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ShowDetailsAccount(name: String, userName: String, description: String) {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.ic_account_icon),
+            contentDescription = "foto do usuário",
+            modifier = Modifier
+                .size(60.dp)
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+        )
+
+        Text(text = name)
+        Text(text = userName)
+        Text(text = description)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    DevhubTheme {
-        Greeting("Android")
+    DevHubTheme {
+        ShowDetailsAccount(
+            name = "Michelle Brito",
+            userName = "michellebritoo",
+            description = "Android dev"
+        )
     }
 }
--- app/src/main/java/br/com/michellebrito/devhub/ui/theme/Theme.kt
+++ app/src/main/java/br/com/michellebrito/devhub/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun DevhubTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun DevHubTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
